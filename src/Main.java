7c383864b8ad1a486d0b0b84f4885b785e7a82e8
MR Taumal <taumal@qail.com.bd>
2024-12-01 23:47:27 +0600

    happening of start method run

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
 class Task implements Runnable {
     @Override
     public void run() {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 1000000; i++) {
             System.out.println(i + ": " + Thread.currentThread().getName());
+
+            try {
+                Thread.sleep(10000L);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
@@ -14,25 +20,27 @@ class Task2 extends Thread {
 
     @Override
     public void run() {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 100000; i++) {
             System.out.println(i + ": " + Thread.currentThread().getName());
         }
     }
 }
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("Start: " + Thread.currentThread().getName());
 
         Task task = new Task();
         Thread t = new Thread(task, "MRT! The Alien");
         t.start();
 
-        Task2 t2 = new Task2();
+        Thread.sleep(10000000L);
+
+        /*Task2 t2 = new Task2();
         t2.start();
 
         for (int i = 0; i < 10; i++) {
             System.out.println(i + ": " + Thread.currentThread().getName());
-        }
+        }*/
 
         System.out.println("End: " + Thread.currentThread().getName());
     }
