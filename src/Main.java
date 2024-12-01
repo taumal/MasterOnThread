4b508e3c0c9971b5a2f15d0ddf54aed5348d80ba
MR Taumal <taumal@qail.com.bd>
2024-12-01 22:37:04 +0600

    thread

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,18 @@
-
+class Task implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i + ": " + Thread.currentThread().getName());
+        }
+    }
+}
 public class Main {
     public static void main(String[] args) {
         System.out.println("Start: " + Thread.currentThread().getName());
 
-        Runnable target = new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 10; i++) {
-                    System.out.println(i + ": " + Thread.currentThread().getName());
-                }
-            }
-        };
-
-        Thread t = new Thread(target);
-        t.start();
+        Task task = new Task();
+        Thread t = new Thread(task, "MRT! The Alien");
+        t.run();
 
         for (int i = 0; i < 10; i++) {
             System.out.println(i + ": " + Thread.currentThread().getName());
