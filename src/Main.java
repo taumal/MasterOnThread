7bf86db95f4243472e44983b7f5a02b9ea12d11a
MR Taumal <taumal@qail.com.bd>
2024-12-01 22:44:29 +0600

    create thread by extending thread

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,29 @@ class Task implements Runnable {
         }
     }
 }
+
+class Task2 extends Thread {
+    public Task2() {
+        super("The Alien");
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i + ": " + Thread.currentThread().getName());
+        }
+    }
+}
 public class Main {
     public static void main(String[] args) {
         System.out.println("Start: " + Thread.currentThread().getName());
 
         Task task = new Task();
         Thread t = new Thread(task, "MRT! The Alien");
-        t.run();
+        t.start();
+
+        Task2 t2 = new Task2();
+        t2.start();
 
         for (int i = 0; i < 10; i++) {
             System.out.println(i + ": " + Thread.currentThread().getName());
